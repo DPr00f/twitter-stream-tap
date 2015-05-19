a880322c11e1ec53187f835d7191e8bd098556e1
Joao Lopes <joao.lopes@gamesys.co.uk>
2015-05-19 18:04:18 +0100

    Removed useless console.log

--- src/app/Setup/Server.js
+++ src/app/Setup/Server.js
@@ -5,8 +5,6 @@ export default class Server {
 
   start(serverPort = 3000) {
     let server = this.app.listen(serverPort, ()=> {
-      console.log(this);
-      
       let host = server.address().address;
       let port = server.address().port;
 
